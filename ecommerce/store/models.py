808ea6ba1cfd3af82eef82eadc3eff4af2c9d8bd
Suraj Rajbanshi <soorajrajbanshi@cagtu.com>
2023-06-28 17:54:08 +0545

    new update

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -19,6 +19,15 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
 class Order(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL, blank=True,null=True)
     date_orderd = models.DateTimeField(auto_now_add= True)
@@ -28,12 +37,39 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
+
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 class OrderItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
     order = models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
     quantity = models.IntegerField(default=0,null=True,blank=True)
     date_orderd = models.DateTimeField(auto_now_add=True)
 
+
+    @property
+    def get_total(self):
+        total = self.product.price*self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer =  models.ForeignKey(Customer,on_delete = models.SET_NULL,blank=True,null=True)
